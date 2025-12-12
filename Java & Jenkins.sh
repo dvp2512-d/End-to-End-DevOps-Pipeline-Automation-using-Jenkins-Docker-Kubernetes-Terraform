286a16c3533e84eebb70c7b87d8d23f8f8d1c59c
DHRUVIL PATEL <pateldvp84472@gmail.com>
2025-12-12 14:55:34 +0530

    Update Java & Jenkins.sh

---
+++ Java & Jenkins.sh
@@ -0,0 +1,10 @@
+sudp apt update
+sudo apt --upgrade -y
+sudo apt install openjdk-17-jdk -y
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
